4bd8aee18c64b2f078ab74658ebc7e65568645ae
Lukasz Kwiecinski <lukkwie1811@gmail.com>
2021-09-24 21:36:54 +0200

    Change default port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Variables
 const REST_API_URL = "https://vulcan-websocket-api.herokuapp.com/";
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Load view engine
 app.set("views", path.join(__dirname, "views"));
